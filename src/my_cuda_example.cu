65a9181f6cda62a71018a7062f9faa682347d028
Adrien Hamraoui <adrien@sogilis.com>
2015-09-22 15:17:12 +0200

    1ère session, la version gpu rocks

--- src/my_cuda_example.cu
+++ src/my_cuda_example.cu
@@ -1,39 +1,64 @@
 #include "my_cuda_example.h"
+#include <ctime>
+#include <stdio.h>
 
-/*************************************************************/
-__device__ int my_kernel_helper(int a, int b)
+typedef struct
 {
-    return a*b;
-}
+    float speed_x, speed_y;
+    float pos_x, pos_y;
+} particle;
 /*************************************************************/
-__global__ void my_kernel()
+__global__ void my_kernel(particle* particles)
 {
-    // extern __shared__ int my_dyn_shared_mem[];
-    //        __shared__ int my_stat_shared_mem[42];
-    //        __shared__ int shared_var;
-    //
-    // blockIdx.x
-    // blockDim.x
-    // threadIdx.x
-    //
-    // __syncthreads();
-    //
-    // atomicAdd(&shared_var, 167);
+    particle *p = &particles[blockIdx.x*blockDim.x + threadIdx.x];
+    p->pos_x += p->speed_x;
+    p->pos_y += p->speed_y;
 }
 /*************************************************************/
 void run_on_cuda()
 {
-    float* device_pointer;
-    int size = 1024*sizeof(float);
+    int n = 1024*1024*100;
 
+    particle* host_pointer, *device_pointer;
+    int size = n*sizeof(particle);
+
+    //allocation
+    host_pointer = (particle*)malloc(size);
     cudaMalloc(&device_pointer, size);
-    //
-    // cudaMemcpy(device_pointer, host_pointer, size, cudaMemcpyHostToDevice);
-    //
-    // my_kernel<<<a, b, c>>>();
-    //
-    // cudaMemcpy(host_pointer, device_pointer, size, cudaMemcpyDeviceToHost);
-    //
+
+    //fill host data
+    //srand(time(NULL));
+    for(int i=0 ; i<n ; i++)
+    {
+        host_pointer[i].speed_x = 1.0;
+        host_pointer[i].speed_y = 3.2;
+        host_pointer[i].pos_x = 0.0;
+        host_pointer[i].pos_y = 0.0;
+    }
+
+    //send data
+    cudaMemcpy(device_pointer, host_pointer, size, cudaMemcpyHostToDevice);
+
+    //compute
+    #define CUDA_EXEC
+    clock_t t = clock();
+   
+    #ifdef CUDA_EXEC
+        my_kernel<<<n / 1024, 1024>>>(device_pointer);
+    #else
+        for(int i=0 ; i<n ; i++)
+        {
+            particle *p = &host_pointer[i];
+            p->pos_x += p->speed_x;
+            p->pos_y += p->speed_y;
+        }
+    #endif
+    clock_t t2 = clock() - t;
+    printf("%f secs\n", (double)t2 / CLOCKS_PER_SEC);
+    //retrieve data
+    cudaMemcpy(host_pointer, device_pointer, size, cudaMemcpyDeviceToHost);
+
     cudaFree(device_pointer);
+    free(host_pointer);
 }
 /*************************************************************/
